7b2cd32f6c523fbcec431e802b393504e600f22b
IamTumanov <viktor.tumanov@waveaccess.global>
2024-04-11 15:07:34 +0300

    добавлена модалка для создания продукта

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
-import { useEffect, useState } from 'react';
-import {Product} from './components/Product'
-import axios, { AxiosError } from 'axios';
-import { IProduct } from '../public/models';
+import { Loader } from './components/Loader';
+import { Error } from './components/Error';
+import { Product } from './components/Product'
+import { useProducts } from './hooks/products';
+import { Modal } from './components/Modal';
+import { CreateProduct } from './components/createProduct';
+
 
 
 function App() {
-
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts() {
-    try {
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-      setProducts(response.data)
-      setLoading(false)
-    } catch (e: unknown) {
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const {products, loading, error} = useProducts()
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className="text-center">Loading...</p>}
-      {error && <p className="text-center text-red-600">{ error }</p>}
-      {products.map(product => <Product product={product} key={product.id}/>)}
+      {loading && <Loader/> }
+      {error && <Error error={error}/> }
+      {products.map(product => <Product product={product} key={product.id}/>) }
+
+      <Modal title='Create new product'>
+        <CreateProduct/>
+      </Modal>
     </div>
   )
 }
